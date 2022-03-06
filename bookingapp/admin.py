32fd02c4c4c14dead7e825d13170a723672b74e7
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-06 21:44:40 +0000

    Add more bootstrap style

--- bookingapp/admin.py
+++ bookingapp/admin.py
@@ -4,18 +4,25 @@ from .models import Apartment, Guest, Booking, FrontPhoto, AddMorePhotos
 
 @admin.register(Apartment)
 class ApartmentAdmin(SummernoteModelAdmin):
-
+    """ Style for admin page to have more aesthetic view """
     list_display = ('apartment_name', 'price', 'beds_nr')
     summernote_fields = ('description')
-    fields = ( 'front_image', )
 
 
 admin.site.register(Guest)
 @admin.register(Booking)
 class BookingRequest(admin.ModelAdmin):
+    """ Add view for the admin to see booking requests """
 
     list_display = ('booking_num', 'apartment', 'guest_name')
 
-admin.site.register(FrontPhoto)
+
+@admin.register(FrontPhoto)
+class ShowFrontPhoto(SummernoteModelAdmin):
+    """ For admin to see which image has been uploaded """
+    
+    media = 'front_image'    
+
+
 admin.site.register(AddMorePhotos)
 
