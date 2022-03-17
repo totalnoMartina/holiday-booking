93b00c2e872bc47e1c1d0ca993b62169bc1b201c
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-17 22:36:48 +0000

    Add guest account profile page

--- guestprofile/models.py
+++ guestprofile/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.core.validators import RegexValidator  # For formatting the phone number
 from django.contrib.auth.models import User
@@ -10,19 +11,18 @@ class GuestProfile(models.Model):
     full_name = models.CharField(max_length=100, blank=False)
     ph_number = models.CharField(null=False, validators=[RegexValidator(r'^\d{3}-\d{3}-\d{4}$')], max_length=40)
     email = models.EmailField(blank=False)
-    guest_number = models.IntegerField()
     kids = models.BooleanField(default=False, blank=True)
     pets = models.BooleanField(default=False, blank=True)
 
     def __str__(self):
         """ A method to show stored data of Guest """
-        return f'A guest with an id of {self.guest_id},  {self.full_name} with a phone number: {self.ph_number} has been created! Welcome {self.user.username}'
+        return f'A guest with an id of {self.guest_id},  {self.full_name} with a phone number: {self.ph_number} has been created! Welcome {self.guest}'
 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """ Create or update user profile """
     if created:
-        GuestProfile.objects.create(user=instance)
+        GuestProfile.objects.create(guest=instance)
     #  Existing users just save the profile
-    instance.GuestProfile.save()
+    instance.guestprofile.save()
--- guestprofile/views.py
+++ guestprofile/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import GuestProfile
 
 def guests_profile(request):
     """ Display the guests profile """
+    profile = get_object_or_404(GuestProfile, guest=request.user)
     template = 'guestprofile/guest.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
